3fbc5441e47426a6d0efcdafb34ac9e261139143
wojtek <wojciech.podlesny1@gmail.com>
2025-09-28 20:38:52 +0200

    Update routes Home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -35,59 +35,43 @@ export default function Home() {
       setResumes(parsedResumes || []);
       setLoadingResumes(false);
     }
+
     loadResumes()
   }, []);
 
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
 
-  const handleClearResumes = async () => {
-      setLoadingResumes(true);
-      const resumesKV = (await kv.list('resume:*', true)) as KVItem[];
-      await Promise.all(resumesKV.map(resume => kv.delete(resume.key)));
-      setResumes([]);
-      setLoadingResumes(false); };
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        {!loadingResumes && resumes?.length === 0 ? (
+            <h2>No resumes found. Upload your first resume to get feedback.</h2>
+        ): (
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        )}
+      </div>
+      {loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+      )}
 
+      {!loadingResumes && resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
 
-  return (
-      <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-          <Navbar />
-
-          <section className="main-section">
-              <div className="page-heading py-16">
-                  <h1>Track Your Applications & Resume Ratings</h1>
-                  {!loadingResumes && resumes?.length === 0 ? (
-                      <h2>No resumes found. Upload your first resume to get feedback.</h2>
-                  ): (
-                      <h2>Review your submissions and check AI-powered feedback.</h2>
-                  )}
-              </div>
-              {loadingResumes && (
-                  <div className="flex flex-col items-center justify-center">
-                      <img src="/images/resume-scan-2.gif" className="w-[200px]" />
-                  </div>
-              )}
-
-              {!loadingResumes && resumes.length > 0 && (
-                  <>
-                      <div className="resumes-section">
-                          {resumes.map((resume) => (
-                              <ResumeCard key={resume.id} resume={resume}/>
-                          ))}
-                      </div>
-                      <button className="primary-button w-fit text-xl font-semibold mb-4"
-                              onClick={handleClearResumes}
-                      >Clear Resumes
-                      </button>
-                  </>
-              )}
-
-              {!loadingResumes && resumes?.length === 0 && (
-                  <div className="flex flex-col items-center justify-center mt-10 gap-4">
-                      <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
-                          Upload Resume
-                      </Link>
-                  </div>
-              )}
-          </section>
-      </main>
-  )
+      {!loadingResumes && resumes?.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
+          </div>
+      )}
+    </section>
+  </main>
 }
